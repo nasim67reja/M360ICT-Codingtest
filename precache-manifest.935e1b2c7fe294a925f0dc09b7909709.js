ab833cf6371e969c342495434f6a6792903c18c2
nasim67reja <nasimreja67@gmail.com>
2023-01-26 17:39:59 +0600

    Updates

---
+++ precache-manifest.935e1b2c7fe294a925f0dc09b7909709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec05464f596980ffca5d",
+    "url": "/M360ICT-Codingtest/static/css/main.8b34ffc6.chunk.css"
+  },
+  {
+    "revision": "ec05464f596980ffca5d",
+    "url": "/M360ICT-Codingtest/static/js/main.ec05464f.chunk.js"
+  },
+  {
+    "revision": "793870ee69a16eb94c5b",
+    "url": "/M360ICT-Codingtest/static/css/1.078ef16d.chunk.css"
+  },
+  {
+    "revision": "793870ee69a16eb94c5b",
+    "url": "/M360ICT-Codingtest/static/js/1.793870ee.chunk.js"
+  },
+  {
+    "revision": "8970ad656590b0092830",
+    "url": "/M360ICT-Codingtest/static/js/runtime~main.8970ad65.js"
+  },
+  {
+    "revision": "e13af7187d4914e66bcdd84cd3c8cf1c",
+    "url": "/M360ICT-Codingtest/index.html"
+  }
+];
