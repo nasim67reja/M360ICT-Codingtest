4dc546b682976593f0a84b3a90139ad177458c10
nasim67reja <nasimreja67@gmail.com>
2023-01-27 17:19:00 +0600

    Updates

---
+++ precache-manifest.caed8f23bbcb2936adb4c604f78fc416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28015c4d1e1df5f00767",
+    "url": "/M360ICT-Codingtest/static/css/main.e8d70a89.chunk.css"
+  },
+  {
+    "revision": "28015c4d1e1df5f00767",
+    "url": "/M360ICT-Codingtest/static/js/main.28015c4d.chunk.js"
+  },
+  {
+    "revision": "7282fa28656f627155c5",
+    "url": "/M360ICT-Codingtest/static/css/1.4455dbb0.chunk.css"
+  },
+  {
+    "revision": "7282fa28656f627155c5",
+    "url": "/M360ICT-Codingtest/static/js/1.7282fa28.chunk.js"
+  },
+  {
+    "revision": "8970ad656590b0092830",
+    "url": "/M360ICT-Codingtest/static/js/runtime~main.8970ad65.js"
+  },
+  {
+    "revision": "9a273b1968f8eaa1d856140285896b0c",
+    "url": "/M360ICT-Codingtest/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/M360ICT-Codingtest/precache-manifest.1024c401c8c03a236bcd8a8673b09eb3.js"
+  "/M360ICT-Codingtest/precache-manifest.caed8f23bbcb2936adb4c604f78fc416.js"
 );
 
 workbox.clientsClaim();
